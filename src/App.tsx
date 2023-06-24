0edaf453442b2b77b2dbcaa11e1f8e0ddc752789
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-06-24 23:53:48 +0500

    feat: "add polymorphic button"

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+// eslint-disable-next-line import/no-unresolved
+
+import { Button } from './components/ui/button/button.tsx'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <Button variant={'primary'} as="a" href={'/link'}>
+        Hello
+      </Button>
+      <Button variant={'primary'}>Hello</Button>
+    </div>
+  )
 }

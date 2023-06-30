05ece277cb30e1a85575213a626aa8eb5d92b7e8
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-06-30 14:25:31 +0500

    feat: add universal modal

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import LogOutIcon from '@/assets/icons/LogOutIcon.tsx'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { Checkbox } from '@/components/ui/checkbox'
+import { Modal } from '@/components/ui/modals'
 import { TextField } from '@/components/ui/text-field'
 import { Typography } from '@/components/ui/typography/typography.tsx'
 
 export function App() {
+  // const [openModal, setOpenModal] = useState(false)
   const [checked, setChecked] = useState<boolean>(false)
   // const checkedHandler = () => {
   //   setChecked(!checked)
@@ -22,6 +24,20 @@ export function App() {
 
   return (
     <div className={s.testDiv}>
+      <Modal
+        renderTriggerButton={() => <Button>Trigger Button</Button>}
+        renderCancelButton={() => <Button>Cancel</Button>}
+        renderActionButton={() => <Button>Save</Button>}
+        contentSeparator={true}
+        showCloseButton={true}
+        title={'test modal'}
+        size={'md'}
+      >
+        <TextField label={'first'} />
+        <TextField label={'second'} />
+        <TextField label={'third'} />
+      </Modal>
+
       <TextField
         value={value}
         placeholder={'placeholder'}
---
+++ src/assets/icons/Close24.tsx
@@ -0,0 +1,19 @@
+import { SVGProps, memo } from 'react'
+const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="none" {...props}>
+    <g clipPath="url(#a)">
+      <path
+        fill={props.color ? props.color : '#fff'}
+        d="m13.4 12 4.3-4.3a1 1 0 1 0-1.4-1.4L12 10.6 7.7 6.3a1 1 0 0 0-1.4 1.4l4.3 4.3-4.3 4.3A1 1 0 0 0 7 18a1 1 0 0 0 .7-.3l4.3-4.3 4.3 4.3a1 1 0 1 0 1.4-1.4L13.4 12Z"
+      />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="#fff" d="M0 0h24v24H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+)
+const Close24 = memo(SvgComponent)
+
+export default Close24

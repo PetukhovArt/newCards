d671a49c865f11ebf494d978e13b35c81716fa89
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-07-06 13:53:32 +0300

    feat: edit app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
 import s from './app.module.scss'
 
@@ -8,8 +8,16 @@ export function App() {
   return (
     <div className="App">
       <Header isLoggedIn={false} name={'ORK'} avatar={''} />
-      <div className={s.main}>
-        <Outlet />
+      <div className={s.container}>
+        <div className={s.main}>
+          <Outlet />
+        </div>
+      </div>
+      <div className={s.navigation}>
+        <NavLink to={'/profile'}>profile</NavLink>
+        <NavLink to={'/register'}>register</NavLink>
+        <NavLink to={'/forgot-password'}>forgot password</NavLink>
+        <NavLink to={'/check-email'}>check email</NavLink>
       </div>
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { App } from '@/app/App.tsx'
 import { store } from '@/app/store.ts'
 import { CheckEmailPage } from '@/features/user/pages/check-email-page/check-email-page.tsx'
+import { ForgotPasswordPage } from '@/features/user/pages/forgot-password-page/forgot-password-page.tsx'
+import { ProfilePage } from '@/features/user/pages/profile-page'
 import { RegisterPage } from '@/features/user/pages/register-page'
 
 const router = createBrowserRouter([
@@ -17,12 +19,20 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
+        path: 'profile',
+        element: <ProfilePage />,
+      },
+      {
         path: 'check-email',
         element: <CheckEmailPage />,
       },
       {
         path: 'register',
         element: <RegisterPage />,
+      },
+      {
+        path: 'forgot-password',
+        element: <ForgotPasswordPage />,
       },
     ],
   },

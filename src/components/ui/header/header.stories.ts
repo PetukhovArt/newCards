2d16216fd8837a84df0dc5581ef3c6d291f6afa2
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-07-08 17:46:23 +0300

    feat: update dropdown, add profile info

--- src/components/ui/header/header.stories.ts
+++ src/components/ui/header/header.stories.ts
@@ -21,6 +21,7 @@ export const HeaderWithButton: Story = {
     isLoggedIn: false,
     name: 'User',
     avatar: 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80',
+    email: 'testmail@gmail.com',
   },
 }
 
@@ -29,5 +30,6 @@ export const HeaderWithUser: Story = {
     isLoggedIn: true,
     name: 'User',
     avatar: 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80',
+    email: 'testmail@gmail.com',
   },
 }

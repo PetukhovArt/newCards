9d59057183e12ba353833ff94dd8b16a64bcd508
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-07-13 21:56:36 +0300

    feat: change size of check icon svg

--- src/assets/icons/check.tsx
+++ src/assets/icons/check.tsx
@@ -3,9 +3,9 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill={props.color}
-    height={24}
+    height={20}
     viewBox="0 -960 960 960"
-    width={24}
+    width={20}
     {...props}
   >
     <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />

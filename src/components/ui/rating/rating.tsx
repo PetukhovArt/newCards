f5bf93bee6aa77bfa9d425a0a3c25f292d10da38
Kozlov Valentin <kozlov0020@gmail.com>
2023-07-22 15:40:53 +0500

    feat: add onchange in rating for change value star onclick

--- src/components/ui/rating/rating.tsx
+++ src/components/ui/rating/rating.tsx
@@ -1,21 +1,30 @@
+import { useState } from 'react'
+
 import s from './rating.module.scss'
 
 import StarIcon from '@/assets/icons/star-icon.tsx'
-import StarOutlineIcon from '@/assets/icons/star-outline-icon.tsx'
 
 type RatingPropsType = {
   value: number
+  onChange?: (value: number) => void
 }
-export const Rating = ({ value }: RatingPropsType) => {
-  const ratingValue = Math.round(value)
+export const Rating = ({ value, onChange }: RatingPropsType) => {
+  const [ratingValue, setRatingValue] = useState(Math.round(value))
+
+  const handleRatingChange = (newValue: number) => {
+    setRatingValue(newValue)
+    if (onChange) {
+      onChange(newValue)
+    }
+  }
 
   return (
     <div className={s.rating}>
-      {ratingValue > 0 ? <StarIcon /> : <StarOutlineIcon />}
-      {ratingValue >= 2 ? <StarIcon /> : <StarOutlineIcon />}
-      {ratingValue >= 3 ? <StarIcon /> : <StarOutlineIcon />}
-      {ratingValue >= 4 ? <StarIcon /> : <StarOutlineIcon />}
-      {ratingValue >= 5 ? <StarIcon /> : <StarOutlineIcon />}
+      <StarIcon onClick={() => handleRatingChange(1)} filled={ratingValue >= 1} />
+      <StarIcon onClick={() => handleRatingChange(2)} filled={ratingValue >= 2} />
+      <StarIcon onClick={() => handleRatingChange(3)} filled={ratingValue >= 3} />
+      <StarIcon onClick={() => handleRatingChange(4)} filled={ratingValue >= 4} />
+      <StarIcon onClick={() => handleRatingChange(5)} filled={ratingValue >= 5} />
     </div>
   )
 }
